d708da9a9ea126f81d524a039ee23052310dd886
Muhammed Aslam <aslam91501@gmail.com>
2022-06-21 22:21:26 +0530

    Spring Security Settings added

--- src/main/java/sdb/server/auth/config/SecurityConfig.java
+++ src/main/java/sdb/server/auth/config/SecurityConfig.java
@@ -2,11 +2,8 @@ package sdb.server.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.RequiredArgsConstructor;
